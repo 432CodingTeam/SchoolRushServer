198f12cccd2933e271047edae3e5a74dc39cb011
Your Name <you@example.com>
2018-04-16 21:13:52 +0800

    添加follow和operation类

---
+++ src/app/Api/follow.php
@@ -0,0 +1,167 @@
+<?php
+namespace App\Api;
+
+use PhalApi\Api;
+use App\Model\Follow as FollowModel;
+/**
+ * 关注接口类
+ *
+ * @author: ssh
+ */
+
+class Follow extends Api {
+
+	public function getRules() {
+        return array(
+            'index' => array(
+                'username' 	=> array('name' => 'username'),
+            ),
+            'add' => array(
+                'uid' => array('name' => "uid"),
+                'type'=>array('name'=>"type"),
+                'target' => array('name' => "target"),
+            ),
+            'getById' => array(
+                'id' => array("name" => "id")
+            ),
+            'deleteById' => array(
+                'id' => array("name" => "id")
+            ),
+            'updateById' => array(
+                'id' => array('name'=> 'id','require'=>true),
+                'uid' => array('name' => "uid",'require'=>true),
+                'type'=>array('name'=>"type",'require'=>true),
+                'target' => array('name' => "target",'require'=>true),
+            ),
+        );
+	}
+	
+	/**
+	 * 默认接口服务
+     * @desc 默认接口服务，当未指定接口服务时执行此接口服务
+	 * @return string title 标题
+	 * @return string content 内容
+	 * @return string version 版本，格式：X.X.X
+	 * @return int time 当前时间戳
+	 */
+	public function index() {
+        return array(
+            'title' => 'Hello ' . $this->username,
+            'version' => PHALAPI_VERSION,
+            'time' => $_SERVER['REQUEST_TIME'],
+        );
+    }
+
+    /**
+     * 获取所有内容
+     * @desc 获取所有学校信息
+     * 
+     * @return array data 所有学校
+     * 
+     */
+    public function getAll() {
+        $model = new FollowModel();
+        $data = $model->getAll();
+        //循环每一行 添加label与value
+        $res = array();
+        while ($row = $data->fetch()) {
+            $row["value"] = $row["id"];
+            $row["label"] = $row["name"];
+            array_push($res, $row);
+        }
+        return $res;
+    }
+
+    /**
+     * 根据ID获取
+     * @desc 根据ID获取用户关注信息
+     * @param int id 要获取的内容的id
+     * 
+     * @return data id 该id指定的内容
+     */
+    public function getById() {
+        $model = new FollowModel();
+        $data = $model->getById($this->id);
+
+        return $data;
+    }
+
+    /**
+     * 根据ID删除
+     * @desc 根据ID删除用户关注信息
+     * 
+     * @param int id 要删除的的id
+     * 
+     * @return int data 删除的id
+     */
+    public function deleteById(){
+        $model =new FollowModel();
+        $data  =$model->deleteById($this->id);
+
+        return $data;
+    }
+
+    /**
+     * 增加用户关注信息
+     * @desc 增加用户关注的信息
+     * 
+     * @param int uid 用户ID
+     * @param int type 关注标签 1 关注用户 2 关注标签
+     * @param int target 被关注的用户/标签的ID
+     * @return array id 增加的用户关注的标签
+     */
+    public function add() {
+        $insert = array(
+            'uid'=>$this->uid,
+            'type'=>$this->type,
+            'target' => $this->target,
+        );
+
+        $model = new FollowModel();
+
+        $id = $model->add($insert);
+
+        return $id;
+    }
+    /**
+     * 更新用户关注信息
+     * @desc 根据id更新用户关注的信息
+     * @param int uid 用户ID
+     * @param int type 关注标签 1 关注用户 2 关注标签
+     * @param int target 被关注的用户/标签的ID
+     * @return data id 更新后的用户关注的信息
+     */
+     
+    public function updateById() {
+        $model = new FollowModel();
+
+        $data = array(
+            "id" => $this->id,
+            'uid'=>$this->uid,
+            'type'=>$this->type,
+            'target' => $this->target,
+        );
+
+        foreach($data as $key => $val) {
+            if($val == NULL){
+                $keys = array_keys($data);
+                $index = array_search($key, $keys);
+
+                array_splice($data, $index, 1);
+            }
+        }
+
+        $id = $model->updateById($this->id,$data);
+        return array("res"=>$id);
+
+    }
+    
+    /**
+     * 获取用户关注总数
+     */
+    public function getTotalNum(){
+        $model = new FollowModel();
+        return $model->getTotalNum();
+    }
+}
+
